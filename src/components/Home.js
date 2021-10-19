d34edd2db4e19554b1b7b9d84dfb7eaed5209dd7
pialee517 <pialee517@gmail.com>
2021-10-19 19:58:37 -0400

    Add Viewers

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
+import ImageSlider from './ImageSlider'
+import Viewers from './Viewers'
+import Movies from './Movies'
 
 function Home() {
     return (
         <Container>
-            Home
+            <ImageSlider />
+            <Viewers />
+            <Movies />
         </Container>
     )
 }
@@ -15,6 +20,7 @@ const Container = styled.main`
     min-height:calc(100vh - 70px);
     padding:0 calc(3.5vw + 5px);
     position:relative;
+    overflow-x:hidden;
     &:before{
         background: url("/images/home-background.png") center center / cover no-repeat fixed;
         content: "";
